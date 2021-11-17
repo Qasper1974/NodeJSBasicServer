d2dda39424385551cd6096d643c9f82314744966
Yves Faems <yves.faems@gmail.com>
2021-11-16 18:57:41 -0800

    add path /

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    res.setHeader('Content-Type', 'text/html')
-    console.log(req.url, req.method, res)
-    res.write('<html>')
-    res.write('<head>')
-    res.write('<title>My First Page')
-    res.write('</title>')
+
+    const url = req.url;
+
+    if (url === '/') {
+        res.write('<html><head>')
+        res.write('<title>Enter Message</title>')
+        res.write('</head>')
+        res.write('<body>')
+        res.write('<form action="/message" method="POST">')
+        res.write('<input type="text" name="message"></input>')
+        res.write('<button type="submit">Send</button></form>')
+        res.write('</body></html>')
+        return res.end()    
+    }
+
+    res.write('<html><head>')
+    res.write('<title>First Page</title>')
     res.write('</head>')
     res.write('<body>')
-    res.write('<h2>Hello world!')
-    res.write('</h2>')
-    res.write('</body>')
-    res.write('</html>')
+    res.write('<h2>Hello world!</h2>')
+    res.write('</body></html>')
+    res.end()    
 
 })
 
 server.listen(8080);
-

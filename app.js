d93113200b090b17d336361ca45aa87e2b6665f5
Yves Faems <yves.faems@gmail.com>
2021-11-18 15:47:23 -0800

    install npm and nodemon

--- app.js
+++ app.js
@@ -1,49 +1,6 @@
 const http = require('http');
-const fs = require('fs')
+const routes = require('./routes')
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer(routes);
 
-    const url = req.url;
-    const method = req.method;
-
-    if (url === '/') {
-        res.write('<html><head>')
-        res.write('<title>Enter Message</title>')
-        res.write('</head>')
-        res.write('<body>')
-        res.write('<form action="/message" method="POST">')
-        res.write('<input type="text" name="message"></input>')
-        res.write('<button type="submit">Send</button></form>')
-        res.write('</body></html>')
-        return res.end()    
-    }
-
-    if (url === '/message' && method === 'POST') {
-
-        const body = []
-
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        });
-
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt',message, err => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-        });
-    }
-    res.write('<html><head>')
-    res.write('<title>First Page</title>')
-    res.write('</head>')
-    res.write('<body>')
-    res.write('<h2>Hello world!</h2>')
-    res.write('</body></html>')
-    res.end()    
-
-})
-
-server.listen(8080);
+server.listen(3000); 

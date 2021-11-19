c321793dca1db8d4868b1eedee05842eb60cbb38
Yves Faems <yves.faems@gmail.com>
2021-11-18 18:50:48 -0800

    import express.js

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const http = require('http');
-const routes = require('./routes')
+const express = require('express');
 
-const server = http.createServer(routes);
+const app = express();
 
-server.listen(3000); 
+app.use((req, res, next) => {
+    console.log('Hier zijn we gepasseerd.');
+    next();
+} )
+
+app.use((req, res, next) => {
+    console.log('Hier zijn we ook nog een keertje gepasseerd.');
+    res.send('<h2>Hoi hoi hoi!</h2>');
+} )
+
+app.listen(3000); 

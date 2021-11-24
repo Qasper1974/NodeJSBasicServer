9deb162da1687b7268ba5a6f39084fcd992c863b
Yves Faems <yves.faems@gmail.com>
2021-11-23 19:21:44 -0800

    create rootDirectory

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const rootDir = require('./utilities/path');
+
 const express = require('express');
 
 const bodyparser = require('body-parser')
@@ -15,7 +17,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use('/', (req, res, next)=>{
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
 })
 
 app.listen(3000); 
--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,13 @@
 const path = require('path');
 
+const rootDir = require('../utilities/path');
 
 const express = require('express')
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../','views', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 } )
 
 module.exports = router;

35496ea91d312d8a7d5c87b0230b6f0c931ed98e
Yves Faems <yves.faems@gmail.com>
2021-11-29 16:14:22 -0800

    template with ejs

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
 const products = adminData.products;  
-res.render('shop.ejs', {
+res.render('shop', {
     prods : products,
     pageTitle : 'Shop',
     path: '/',

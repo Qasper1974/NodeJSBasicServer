0af63758f6edfe9d7f4dc524b6fd1d2346b54b24
Yves Faems <yves.faems@gmail.com>
2021-12-02 15:35:39 -0800

    add controllers and models

--- app.js
+++ app.js
@@ -12,18 +12,18 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const shopRoutes = require('./routes/shop')
-const adminData = require('./routes/admin')
+const adminRoutes = require('./routes/admin')
+
+const errorController = require('./controllers/error')
 
 app.use(bodyparser.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use('/', (req, res, next)=>{
-    res.status(404).render('404', { pageTitle : 'Page not found'} );
-})
+app.use(errorController.get404)
 
 app.listen(3000); 
 
